70fdc56bf387c8a43f5faa85ccc6984f8b766b27
David Haygarth <david.haygarth@gmail.com>
2022-05-26 16:46:58 +0100

    updated jest.config.js to use jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  roots: ['src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testPathIgnorePatterns: ['node_modules/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/*.test.(ts|tsx)'],
   moduleNameMapper: {
-    '.(css|less|sass)$': 'identity-obj-proxy',
+    // Mocks out all these file formats when tests are run.
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'identity-obj-proxy',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
